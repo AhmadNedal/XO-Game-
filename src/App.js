7d23b9c8515b506c2e564128d69385b4ee49b87f
Ahmad Nedall <ahmadnedal808@gmail.com>
2025-04-01 16:19:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [name1 , setName1 ] = useState("Player1"); 
   const [name2 , setName2 ] = useState("Player2"); 
   const [Same , setSame ] = useState(0); 
+  const [win , setWin ] = useState(-1); 
 
   const HandelClick=(idx)=> {
 
@@ -23,10 +24,10 @@ function App() {
         setReserved(newReserved) ; 
         setTurn(!turn);
         setBox(newBox); 
-        setSame(Same+1) ;
+        setSame(Same =>Same+1) ;
       }
 
-      if ( Same>=8 ){
+      if (Same>=8){
         setShow(true) ; 
       }
 
@@ -39,10 +40,11 @@ function App() {
       <>
       <table>
           <tr onClick={()=>{
-            if ( (turn==box[4]&&turn==box[8]) || (turn==box[1]&&turn==box[2]) ||(turn==box[3]&&turn==box[6]) )  {
-                setShow(true)
-            }
             HandelClick(0); 
+            if ( (turn==box[4]&&turn==box[8]) || (turn==box[1]&&turn==box[2]) ||(turn==box[3]&&turn==box[6]) )  {
+                setShow(true); 
+                setWin(true) ; 
+            }
           }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[0])?"block":"none"}} src={require((!box[0])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -50,10 +52,11 @@ function App() {
           </tr>
 
             <tr onClick={()=>{
-              if ( (turn==box[0]&&turn==box[2]) || (turn==box[4]&&turn==box[7])  )  {
-                setShow(true)
-              }
               HandelClick(1) ; 
+              if ( (turn==box[0]&&turn==box[2]) || (turn==box[4]&&turn==box[7])  )  {
+                setShow(true) ; 
+                setWin(true) ; 
+              }
             }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[1])?"block":"none"}} src={require((!box[1])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -62,10 +65,11 @@ function App() {
 
 
             <tr onClick={()=>{
-              if ( (turn==box[0]&&turn==box[1]) || (turn==box[5]&&turn==box[8]) || (turn==box[4]&&turn==box[6])  )  {
-                setShow(true)
-              }
               HandelClick(2) ;
+              if ( (turn==box[0]&&turn==box[1]) || (turn==box[5]&&turn==box[8]) || (turn==box[4]&&turn==box[6])  )  {
+                setShow(true) ; 
+                setWin(true) ; 
+              }
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[2])?"block":"none"}} src={require((!box[2])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -75,30 +79,34 @@ function App() {
             <br />
   
             <tr onClick={()=>{
-              if ( ((turn==box[4]&&turn==box[5]) || (turn==box[0]&&turn==box[6]))   )  {
-                setShow(true)
-              } 
               HandelClick(3); 
+              if ( ((turn==box[4]&&turn==box[5]) || (turn==box[0]&&turn==box[6]))   )  {
+                setShow(true) ;
+                setWin(true) ;
+              } 
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[3])?"block":"none"}} src={require((!box[3])?'./images/oo.png':'./images/xx.png')} alt="" />  
               </div>
             </tr>
             <tr onClick={()=>{
-              if ( (turn==box[3]&& turn==box[5]) || (turn == box[1]&&turn==box[7]) ||(turn== box[6]&&turn==box[2] ||(turn==box[0]&&turn==box[8]))  )  {
-                setShow(true)
-              }
               HandelClick(4); 
+              if ( (turn==box[3]&& turn==box[5]) || (turn == box[1]&&turn==box[7]) ||(turn== box[6]&&turn==box[2] ||(turn==box[0]&&turn==box[8]))  )  {
+                setShow(true) ;
+                setWin(true) ;
+              }
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[4])?"block":"none"}} src={require((!box[4])?'./images/oo.png':'./images/xx.png')} alt="" />  
               </div>
             </tr>
             <tr onClick={()=>{
-              if ( (turn==box[4]&&turn==box[3]) || (turn==box[2]&&turn==box[8])  )  {
-                setShow(true)
-              }
               HandelClick(5);
+              if ( (turn==box[4]&&turn==box[3]) || (turn==box[2]&&turn==box[8])  )  {
+                setShow(true) ; 
+                setWin(true) ;
+
+              }
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[5])?"block":"none"}} src={require((!box[5])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -106,12 +114,11 @@ function App() {
             </tr>
             <br />
             <tr onClick={()=>{
-              if ( (turn==box[7]&&turn==box[8]) || (turn==box[0]&&turn==box[3]) ||(turn==box[2]&&turn==box[4])  )  {
-                setShow(true)
-              }
-              
               HandelClick(6); 
-              
+              if ( (turn==box[7]&&turn==box[8]) || (turn==box[0]&&turn==box[3]) ||(turn==box[2]&&turn==box[4])  )  {
+                setShow(true) ;
+                setWin(true) ;
+              }
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[6])?"block":"none"}} src={require((!box[6])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -120,11 +127,12 @@ function App() {
             
             <tr onClick={()=>{
               
+              HandelClick(7) ;
               if ( (turn==box[6]&&turn==box[8]) || (turn==box[1]&&turn==box[4])  )  {
-                setShow(true)
+                setShow(true); 
+                setWin(true) ;
               }
               
-              HandelClick(7) ;
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[7])?"block":"none"}} src={require((!box[7])?'./images/oo.png':'./images/xx.png')} alt="" />  
@@ -132,56 +140,65 @@ function App() {
             </tr>
             
             <tr onClick={()=>{
-              
+              HandelClick(8);
               if ( (turn==box[7]&&turn==box[6]) || (turn==box[0]&&turn==box[4]) ||(turn==box[2]&&turn==box[5])  )  {
-                setShow(true)
+                setShow(true);
+                setWin(true) ;
               }
-              HandelClick(8); 
-              
               }}>
               <div className='Box1'> 
                  <img className='image' style={{ display:(reserved[8])?"block":"none"}} src={require((!box[8])?'./images/oo.png':'./images/xx.png')} alt="" />  
               </div>
             </tr>
-  
           </table>
-            
           </>
     )
   }
 
   const End = ()=> {
       return (
-        <>
-
-        {Same>=8?
-        <h1 className="text-2xl font-bold text-gray-700 bg-yellow-200 p-4 rounded-lg text-center shadow-md">
-        🤝 تعادل!
+        <div className='text-green-600 bg-green-100 p-4 rounded-lg text-center shadow-md'>
+        {(Same>=9&&win==-1)?
+        <h1 className="text-2xl font-bold text-gray-700 p-4 rounded-lg text-center ">
+        تعادل
       </h1>
-      
-      :
-        
-        
-        <h1 className="text-2xl font-bold text-green-600 bg-green-100 p-4 rounded-lg text-center shadow-md">
-  🎉  "{!turn ? name1 : name2}" ألف مبرووك!
-       </h1>
-       }
+      :  
+        <h1 className="text-2xl font-bold text-green-600  p-4 rounded-lg text-center ">
+        ألف مبرووك  <br/> 
+        " <span style={{color:"red" ,  fontSize:"30px" }}>{!turn ? name1 : name2}</span> " 
+       </h1>}
 
+       <button 
+        class="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition"
+        onClick={()=> {
+          setBox([-1,-1,-1,-1,-1,-1,-1,-1,-1]); 
+          setReserved([0,0,0,0,0,0,0,0,0]); 
+          setTurn(1); 
+          setShow(false); 
+          setStart(false); 
+          setSame(0); 
+        }}
+      >
+        إعادة اللعبة
+      </button>
 
-        </>
-      )
+      <button 
+        class="bg-red-500  text-white px-4 py-2 rounded-lg shadow-md hover:bg-red-600 transition ml-4"
+        onClick={()=> {
+          window.location.reload();
+        }}
+      >
+        تغيير اللاعبين
+      </button>
+        </div>
+      );
   }
-
 
   return (
     <div className="App">
-         
-
          {start? <Start setName1={setName1} setName2={setName2} setStart={setStart}/> :
           show ?<End/>:<Game/> 
-         }
-
-         
+         }         
     </div>
   );
 }
